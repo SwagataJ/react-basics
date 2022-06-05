2e040962992f55eb4a8532968aed144fc3a9de7e
Prabhat <prabhat.namdharani@learner.manipal.edu>
2022-06-05 20:01:04 +0530

    Testimonials created

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,35 +1,25 @@
 import React from 'react'
 import './testimonials.css'
-import img1 from '../../assets/img1.jpg'
-import img2 from '../../assets/img2.jpeg'
-import img3 from '../../assets/img3.jpeg'
+import img1 from '../../assets/blank101.jpeg'
+import img2 from '../../assets/iaeste.jpg'
+import img3 from '../../assets/netlab2.jpg'
 
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/a11y';
 
 const data=[
   {
     image:img1,
-    name:"John Doe",
-    review:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
+    name:"Anusha, Blank 101 President",
+    review:"His performance has been recorded to be excellent within the club, and he has provided constructive feedback to writers     while proving instrumental in improving their writing. He has good leadership and communication abilities and has proven to be a very diligent team worker." 
   },
   {
     image:img2,
-    name:"John Doe",
-    review:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+    name:"Dr. Karunakar Kotegar, IAESTE India President",
+    review:"Prabhat is poised remarkably well in different stages of his ongoing career. He is always quick on his feet with sensible reactions to problems at hand.When it comes to his work he exhibits a great level of professionalism and is notably passionate about his work. His interpersonal skills, intelligence and analytical powers are supplemented by his diligence."
   },
   {
     image:img3,
-    name:"John Doe",
-    review:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+    name:"Amay Gada, Network TechLab",
+    review:"He is a quick learner, and is extremely hardworking, as well as quite enthusiastic throughout the internship and he has produced great results overall. He was a sincere and amiable intern, who was always ready to take constructive criticism, and strives to become better in all aspects. "
   }
 ]
 const testimonials = () => {
@@ -39,16 +29,16 @@ const testimonials = () => {
         Review from Co-Workers
       </h5>
       <h2>
-        Testimonials
+        LORs
       </h2>
 
       <div class="container testimonials__container">
         {
           data.map(({image,name,review})=>{
             return(
-              <div class="testimonials__card">
+              <div class="testimonials__card" >
                 <div class="testimonials__card-image">
-                <img src={image} alt=""/>
+                  <img src={image} alt=""/>
                 </div>
                 <div class="testimonials__card-content">
                 <h3>{name}</h3>
